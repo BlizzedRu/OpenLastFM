6721afa457fb61cfaeb33a90e578e27028b6f99c
BlizzedRu <me@blizzed.ru>
2017-12-04 03:45:04 +0300

    Required params bug fixed

--- src/main/java/ru/blizzed/openlastfm/params/Param.java
+++ src/main/java/ru/blizzed/openlastfm/params/Param.java
@@ -58,7 +58,7 @@ public class Param<DataType> {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Param)) return false;
         Param param = (Param) o;
         return Objects.equals(name, param.name);
     }
